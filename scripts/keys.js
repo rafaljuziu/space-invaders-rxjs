7b4710c4bfa791d526697ff01d0bb03328ed6607
Rafal Jozwik <rafal.jozwik@capgemini.com>
2017-11-19 17:35:57 +0100

    4->3

--- scripts/keys.js
+++ scripts/keys.js
@@ -1,11 +1,6 @@
 import {game} from './game';
 import * as Rx from 'rxjs-es';
 
-const keysPressed = {
-  right: false,
-  left: false
-};
-
 function keyLeft(key) {
   return key === 'ArrowLeft';
 }
@@ -18,40 +13,16 @@ function keyFire(key) {
   return key === 'Space';
 }
 
-let keyDown$ = Rx.Observable.fromEvent(document, 'keydown')
-  .map(keyEvent => keyEvent.code);
-
-keyDown$.filter(keyLeft)
-  .subscribe(() => {
-    keysPressed.left = true;
-  });
-
-keyDown$.filter(keyRight)
-  .subscribe(() => {
-    keysPressed.right = true;
-  });
-
-let keyUp$ = Rx.Observable.fromEvent(document, 'keyup')
-  .map(keyEvent => keyEvent.code);
-
-keyUp$.filter(keyLeft)
-  .subscribe(() => {
-    keysPressed.left = false;
-  });
-
-keyUp$.filter(keyRight)
-  .subscribe(() => {
-    keysPressed.right = false;
-  });
-
-let playerMove = Rx.Observable.interval(50).map(() => keysPressed);
-
-playerMove.filter(keys => keys.right && !keys.left)
-  .subscribe(() => {
-    game.move(1);
-  });
-
-playerMove.filter(keys => !keys.right && keys.left)
+Rx.Observable.fromEvent(document, 'keydown')
+  .map(keyEvent => keyEvent.code)
+  .filter(keyLeft)
   .subscribe(() => {
     game.move(-1);
+  });
+
+Rx.Observable.fromEvent(document, 'keydown')
+  .map(keyEvent => keyEvent.code)
+  .filter(keyRight)
+  .subscribe(() => {
+    game.move(1);
   });
